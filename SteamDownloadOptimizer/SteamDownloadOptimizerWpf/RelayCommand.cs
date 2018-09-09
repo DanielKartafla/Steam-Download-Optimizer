66a384e82fcffeacb46abf5e4a707f0e03153d58
Daniel Kartafla <danielkartafla@gmail.com>
2018-09-09 13:01:50 +0200

    Add RelayCommand class

---
+++ SteamDownloadOptimizer/SteamDownloadOptimizerWpf/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SteamDownloadOptimizerWpf
+{
+  public class RelayCommand : ICommand
+  {
+    private Action m_Action;
+
+    public RelayCommand(Action action)
+    {
+      m_Action = action;
+    }
+
+    public void Execute(object parameter)
+    {
+      if (m_Action != null)
+      {
+        m_Action();
+      }
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return true;
+    }
+
+    public event EventHandler CanExecuteChanged;
+  }
+}
